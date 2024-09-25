fd7dbb4775271206c2c0f66d86bee7592b4e12e7
benjaminvasquez07 <benjaminvasquez289@gmail.com>
2024-09-25 15:02:34 -0300

    hice la autenticacion

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,36 +1,52 @@
 import { useEffect, useState } from "react";
-import supabase from "./supabaseClient";
+import { supabase } from "./supabaseClient";
 
 export default function App() {
-  const [user, setUser] = useState(null);
+ const [user, setUser] = useState(null);
 
-  useEffect(()=> { //el useeffect nos permite ejecutar el codigo que se encuentre adentro , en el caso de no recibir depencia entre los corchetes el useeffec se ejecutara solamente para cuando inicie la aplicacion
-    const getSession = async() => {
-      const { data,error } = await supabase.auth.getSession();
-      if (error) {
-        console.log(error);
-      } else {
-        setUser(data?.session?.user);
-      };
-    }
-    
-  getSession();
-},[])
-const handleClick = async () => {
-  const {date,error} = supabase.auth.signInWithOAuth({
-    provider: "github",
-  });
-  if (error) {
-    console.log(error);
-  } else
-  console.log(data);
-};
-return (
-  <>
-  <header>
-    este el header
-    <button onClick={handleClick}>conectar con GitHub</button>
-  </header>
-  </>
-)
+
+ useEffect(() => {
+   const session = supabase.auth.getSession();
+   setUser(session?.user);
+   const {
+     data: { subscription },
+   } = supabase.auth.onAuthStateChange((event, session) => {
+     switch (event) {
+       case "SIGNED_IN":
+         setUser(session?.user);
+         break;
+       case "SIGNED_OUT":
+         setUser(null);
+         break;
+       default:
+     }
+   });
+   return () => {
+     subscription.unsubscribe();
+   };
+ }, []);
+
+
+ const login = async () => {
+   await supabase.auth.signInWithOAuth({
+     provider: "github",
+   });
+ };
+ const logout = async () => {
+   await supabase.auth.signOut();
+ };
+
+
+ return (
+   <div>
+     {user ? (
+       <div>
+         <h1>Authenticated</h1>
+         <button onClick={logout}>Logout</button>
+       </div>
+     ) : (
+       <button onClick={login}>Login with Github</button>
+     )}
+   </div>
+ );
 }
--- blog/src/supabaseClient.js
+++ blog/src/supabaseClient.js
@@ -1,8 +1,6 @@
-import { createClient, creteClient } from "@supabase/supabase-js"
+import { createClient } from "@supabase/supabase-js"
 
-const supabase = createClient(
+export const supabase = createClient(
 "https://swvcaftqqrmxxmjriscz.supabase.co/",
 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3dmNhZnRxcXJteHhtanJpc2N6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY2Nzk0MjgsImV4cCI6MjA0MjI1NTQyOH0.hLz9FQELNoF-Prol8k64rbALnVLl4R_vPyXhGveutwk"
 );
-
-export default supabase;
